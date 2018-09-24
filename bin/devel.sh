33c6c72e4fc3c1e4d37f7e399ce62dd0b2d72489
Goran Mekić <meka@tilda.center>
2018-09-24 15:03:37 +0200

    Check for proper CBSD path

--- bin/devel.sh
+++ bin/devel.sh
@@ -5,12 +5,12 @@ export BIN_DIR=`dirname $0`
 export PROJECT_ROOT=`readlink -f "${BIN_DIR}/.."`
 
 
-if [ -e /usr/local/sbin/cbsd ]; then
-  tmux new-session -s "tilda" -d "${PROJECT_ROOT}/services/backend/bin/devel.sh"
-  tmux splitw -h -p 50 -t 0 "${PROJECT_ROOT}/services/frontend/bin/devel.sh"
-  tmux a
-else
+if [ -e /usr/local/bin/cbsd ]; then
   sudo tmux new-session -s "tilda" -d "cbsd jexec user=devel jname=backend /usr/src/bin/devel.sh"
   sudo tmux splitw -h -p 50 -t 0 "cbsd jexec user=devel jname=frontend /usr/src/bin/devel.sh"
   sudo tmux a
+else
+  tmux new-session -s "tilda" -d "${PROJECT_ROOT}/services/backend/bin/devel.sh"
+  tmux splitw -h -p 50 -t 0 "${PROJECT_ROOT}/services/frontend/bin/devel.sh"
+  tmux a
 fi
