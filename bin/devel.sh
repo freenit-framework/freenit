c19a7702388b78f8633446bab613f0a0e908db42
Goran Mekić <meka@tilda.center>
2019-07-16 13:25:07 +0200

    Initialize backend

--- bin/devel.sh
+++ bin/devel.sh
@@ -8,15 +8,15 @@ export OFFLINE="${OFFLINE:=no}"
 
 if [ -f /usr/local/bin/cbsd ]; then
   backend_hostname=$(sudo cbsd jexec user=devel jname=backend hostname)
-  sudo cbsd jexec user=devel jname=backend env OFFLINE=${OFFLINE} /usr/src/bin/default_user.sh
+  sudo cbsd jexec user=devel jname=backend env OFFLINE=${OFFLINE} /usr/src/bin/init.sh
   sudo tmux new-session -s "web" -d "cbsd jexec user=devel jname=backend env OFFLINE=${OFFLINE} /usr/src/bin/devel.sh"
-  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=frontend env OFFLINE=${OFFLINE} BACKEND_URL="http://${backend_hostname}:5000" /usr/src/bin/devel.sh"
+  sudo tmux split-window -h -p 50 -t 0 "sudo cbsd jexec user=devel jname=frontend env OFFLINE=${OFFLINE} BACKEND_URL=\"http://${backend_hostname}:5000\" /usr/src/bin/devel.sh"
   sudo tmux a -t "web"
 else
   backend_hostname='localhost:5000'
   "${PROJECT_ROOT}/bin/download_repos.sh"
-  env OFFLINE=${OFFLINE} "${PROJECT_ROOT}/services/backend/bin/default_user.sh"
+  env OFFLINE=${OFFLINE} "${PROJECT_ROOT}/services/backend/bin/init.sh"
   tmux new-session -s "web" -d "env OFFLINE=${OFFLINE} ${PROJECT_ROOT}/services/backend/bin/devel.sh"
-  tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} BACKEND_URL="http://${backend_hostname}:5000" ${PROJECT_ROOT}/services/frontend/bin/devel.sh"
+  tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} BACKEND_URL=\"http://${backend_hostname}:5000\" ${PROJECT_ROOT}/services/frontend/bin/devel.sh"
   tmux a -t "web"
 fi
