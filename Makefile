d0d1d256e9e01f2db7cd6864f9f21d9af0683155
Goran Mekić <meka@tilda.center>
2019-10-24 01:48:14 +0200

    Read app name from file

--- Makefile
+++ Makefile
@@ -2,8 +2,6 @@ REGGAE_PATH = /usr/local/share/reggae
 SERVICES = backend https://github.com/mekanix/backend-startkit \
 	   frontend https://github.com/mekanix/frontend-startkit
 
-.include <${REGGAE_PATH}/mk/project.mk>
-
 
 collect: up
 	@bin/collect.sh
@@ -11,5 +9,5 @@ collect: up
 shell:
 	@make -C services/backend shell
 
-print-virtualenv:
-	@make -C services/backend print-virtualenv
+
+.include <${REGGAE_PATH}/mk/project.mk>
--- bin/devel.sh
+++ bin/devel.sh
@@ -3,15 +3,16 @@
 
 export BIN_DIR=`dirname $0`
 export PROJECT_ROOT="${BIN_DIR}/.."
+. "${PROJECT_ROOT}/services/backend/name.py"
 export OFFLINE="${OFFLINE:=no}"
 
 
 if [ -f /usr/local/bin/cbsd ]; then
-  backend_hostname=$(sudo cbsd jexec user=devel jname=backend hostname)
+  backend_hostname=$(sudo cbsd jexec user=devel "jname=${app_name}" hostname)
   frontend_hostname=$(sudo cbsd jexec user=devel jname=frontend hostname)
-  sudo cbsd jexec user=devel jname=backend env OFFLINE=${OFFLINE} /usr/src/bin/init.sh
-  sudo tmux new-session -s "web" -d "cbsd jexec user=devel jname=backend env OFFLINE=${OFFLINE} /usr/src/bin/devel.sh"
-  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=frontend env HOST=${frontend_hostname} OFFLINE=${OFFLINE} BACKEND_URL=\"http://${backend_hostname}:5000\" /usr/src/bin/devel.sh"
+  sudo cbsd jexec user=devel jname="${app_name}back" env OFFLINE=${OFFLINE} /usr/src/bin/init.sh
+  sudo tmux new-session -s "web" -d "cbsd jexec user=devel jname=${app_name}back env OFFLINE=${OFFLINE} /usr/src/bin/devel.sh"
+  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=frontend env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 /usr/src/bin/devel.sh"
   sudo tmux a -t "web"
 else
   backend_hostname='localhost:5000'
