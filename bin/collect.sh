9c2dd4e1d550aa7c2e39abb051f790ac0b5d05e3
Goran Mekić <meka@tilda.center>
2019-10-24 10:54:53 +0200

    Use conf files for names

--- bin/collect.sh
+++ bin/collect.sh
@@ -3,11 +3,15 @@
 
 export BIN_DIR=`dirname $0`
 export PROJECT_ROOT="${BIN_DIR}/.."
+. "${PROJECT_ROOT}/services/backend/name.py"
+export backend_app_name=${app_name}
+. "${PROJECT_ROOT}/services/frontend/name.ini"
+export frontend_app_name=${app_name}
 
 
 if [ -f /usr/local/bin/cbsd ]; then
-  sudo cbsd jexec user=devel jname=backend /usr/src/bin/collect.sh
-  sudo cbsd jexec user=devel jname=frontend /usr/src/bin/collect.sh
+  sudo cbsd jexec user=devel "jname=${backend_app_name}back" /usr/src/bin/collect.sh
+  sudo cbsd jexec user=devel "jname=${frontend_app_name}front" /usr/src/bin/collect.sh
 else
   ${PROJECT_ROOT}/services/backend/bin/collect.sh
   ${PROJECT_ROOT}/services/frontend/bin/collect.sh
--- bin/devel.sh
+++ bin/devel.sh
@@ -4,15 +4,18 @@
 export BIN_DIR=`dirname $0`
 export PROJECT_ROOT="${BIN_DIR}/.."
 . "${PROJECT_ROOT}/services/backend/name.py"
+export backend_app_name=${app_name}
+. "${PROJECT_ROOT}/services/frontend/name.ini"
+export frontend_app_name=${app_name}
 export OFFLINE="${OFFLINE:=no}"
 
 
 if [ -f /usr/local/bin/cbsd ]; then
-  backend_hostname=$(sudo cbsd jexec user=devel "jname=${app_name}" hostname)
-  frontend_hostname=$(sudo cbsd jexec user=devel jname=frontend hostname)
-  sudo cbsd jexec user=devel jname="${app_name}back" env OFFLINE=${OFFLINE} /usr/src/bin/init.sh
-  sudo tmux new-session -s "web" -d "cbsd jexec user=devel jname=${app_name}back env OFFLINE=${OFFLINE} /usr/src/bin/devel.sh"
-  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=frontend env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 /usr/src/bin/devel.sh"
+  backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}" hostname)
+  frontend_hostname=$(sudo cbsd jexec user=devel "jname=${frontend_app_name}front" hostname)
+  sudo cbsd jexec user=devel jname="${backend_app_name}back" env OFFLINE=${OFFLINE} /usr/src/bin/init.sh
+  sudo tmux new-session -s "web" -d "cbsd jexec user=devel jname=${backend_app_name}back env OFFLINE=${OFFLINE} /usr/src/bin/devel.sh"
+  sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=${frontend_app_name}front env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 /usr/src/bin/devel.sh"
   sudo tmux a -t "web"
 else
   backend_hostname='localhost:5000'
