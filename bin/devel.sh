c94f567ebdae28da04ebe130ece5e22342ce50d7
Goran Mekić <meka@tilda.center>
2019-10-25 20:19:18 +0200

    Fix non-reggae development

--- bin/devel.sh
+++ bin/devel.sh
@@ -24,7 +24,7 @@ if [ "${REGGAE}" = "yes" ]; then
   sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=${frontend_app_name}front env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 /usr/src/bin/devel.sh"
   sudo tmux a -t "web"
 else
-  backend_hostname='localhost:5000'
+  backend_hostname='localhost'
   "${BIN_DIR}/download_repos.sh"
   env OFFLINE=${OFFLINE} "${PROJECT_ROOT}/services/backend/bin/init.sh"
   tmux new-session -s "web" -d "env OFFLINE=${OFFLINE} ${PROJECT_ROOT}/services/backend/bin/devel.sh"
