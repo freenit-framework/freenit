c17ec6446a6258de1a224ea541327ffeb97ddbc5
Goran Mekić <meka@tilda.center>
2020-01-27 17:46:42 +0100

    Vagrant support

--- lib.mk
+++ lib.mk
@@ -6,4 +6,3 @@ lib:
 
 lib_mount:
 	@sudo mount -t nullfs "${PWD}/services/lib/dist" "${CBSD_WORKDIR}/jails/freenitfront/usr/src/node_modules/freenit/dist"
-	@sudo mount -t nullfs "${PWD}/services/lib/node_modules" "${CBSD_WORKDIR}/jails/freenitfront/usr/src/node_modules/freenit/node_modules"
