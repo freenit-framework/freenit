f0328aa932c8fe02cbd00498ad3f4f9a304fd467
Goran Mekić <meka@tilda.center>
2020-06-06 16:45:59 +0200

    Backend rename

--- bin/build.sh
+++ bin/build.sh
@@ -10,11 +10,11 @@ export frontend_app_name=${app_name}
 
 
 if [ -f /usr/local/bin/cbsd ]; then
-  sudo cbsd jexec user=devel "jname=${backend_app_name}back" /usr/src/bin/collect.sh
-  sudo cbsd jexec user=devel "jname=${frontend_app_name}front" /usr/src/bin/collect.sh
+  sudo cbsd jexec user=devel "jname=${backend_app_name}" /usr/src/bin/build.sh
+  sudo cbsd jexec user=devel "jname=${frontend_app_name}front" /usr/src/bin/build.sh
 else
-  ${PROJECT_ROOT}/services/backend/bin/collect.sh
-  ${PROJECT_ROOT}/services/frontend/bin/collect.sh
+  ${PROJECT_ROOT}/services/backend/bin/build.sh
+  ${PROJECT_ROOT}/services/frontend/bin/build.sh
 fi
 
 rm -rf ${PROJECT_ROOT}/build/*
--- bin/devel.sh
+++ bin/devel.sh
@@ -18,10 +18,10 @@ fi
 
 
 if [ "${REGGAE}" = "yes" ]; then
-  backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}back" hostname)
+  backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}" hostname)
   frontend_hostname=$(sudo cbsd jexec user=devel "jname=${frontend_app_name}front" hostname)
-  sudo cbsd jexec user=devel jname="${backend_app_name}back" env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} /usr/src/bin/init.sh
-  sudo tmux new-session -s "freenit" -d "cbsd jexec user=devel jname=${backend_app_name}back env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} /usr/src/bin/devel.sh"
+  sudo cbsd jexec user=devel jname="${backend_app_name}" env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} /usr/src/bin/init.sh
+  sudo tmux new-session -s "freenit" -d "cbsd jexec user=devel jname=${backend_app_name} env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} /usr/src/bin/devel.sh"
   sudo tmux split-window -h -p 50 -t 0 "cbsd jexec user=devel jname=${frontend_app_name}front env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 /usr/src/bin/devel.sh"
   sudo tmux a -t "freenit"
 else
