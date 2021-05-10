c0e86406a0583d12249aa80e37cc2f2b255895c9
Goran Mekić <meka@tilda.center>
2021-05-10 09:10:44 +0200

    Run all services

--- bin/devel.sh
+++ bin/devel.sh
@@ -23,10 +23,8 @@ if [ "${REGGAE}" = "yes" ]; then
   backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}" hostname)
   frontend_hostname=$(sudo cbsd jexec user=devel "jname=${frontend_app_name}front" hostname)
   sudo tmux new-session -s "${backend_app_name}" -d "make -C services/backend devel"
-  sudo tmux split-window -h -p 50 -t 0 "make -C services/frontend BACKEND_URL=http://${backend_hostname}:5000 devel"
-  if [ "${LIB}" != "no" ]; then
-    sudo tmux split-window -v -p 50 -t 1 "make -C services/lib BACKEND_URL=http://${backend_hostname}:5000 devel"
-  fi
+  sudo tmux split-window -h -p 66 -t 0 "make -C services/frontend BACKEND_URL=http://${backend_hostname}:5000 devel"
+  sudo tmux split-window -h -p 50 -t 1 "make -C services/startkit BACKEND_URL=http://${backend_hostname}:5000 devel"
   if [ "${DESIGNER}" != "no" ]; then
     sudo tmux split-window -v -p 50 -t 0 "make -C services/designer devel"
   fi
