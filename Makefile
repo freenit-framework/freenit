59627cc320e3db146e0e50ce6e46ef6ba9a195cb
Goran Mekić <meka@tilda.center>
2021-05-22 20:58:54 +0200

    Run only defined services

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 REGGAE_PATH = /usr/local/share/reggae
 SERVICES += backend https://github.com/freenit-framework/backend
 SERVICES += frontend https://github.com/freenit-framework/frontend
-# SERVICES += doc https://github.com/freenit-framework/doc
-# SERVICES += designer https://github.com/freenit-framework/designer
-# SERVICES += cli https://github.com/freenit-framework/cli
+SERVICES += doc https://github.com/freenit-framework/doc
+SERVICES += designer https://github.com/freenit-framework/designer
+SERVICES += cli https://github.com/freenit-framework/cli
 SERVICES += startkit https://github.com/freenit-framework/frontend-startkit
 
 do_devel:
 .if defined(service)
 	@${MAKE} ${MAKEFLAGS} -C services/${service} devel offline=${offline}
 .else
-	@env OFFLINE=${offline} DESIGNER=${DESIGNER} LIB=${LIB} bin/devel.sh reggae
+	@env OFFLINE=${offline} REGGAE=yes bin/devel.sh `make service_names`
 .endif
 
 .include <${REGGAE_PATH}/mk/project.mk>
--- bin/devel.sh
+++ bin/devel.sh
@@ -1,42 +1,43 @@
 #!/bin/sh
 
-
 export BIN_DIR=`dirname $0`
 export PROJECT_ROOT="${BIN_DIR}/.."
 . "${PROJECT_ROOT}/services/backend/name.py"
 export backend_app_name=${app_name}
-. "${PROJECT_ROOT}/services/frontend/name.ini"
-export frontend_app_name=${app_name}
 export OFFLINE=${OFFLINE:="no"}
-export REGGAE="no"
 export SYSPKG=${SYSPKG:="no"}
 export LIB=${LIB:="no"}
 export DESIGNER=${DESIGNER:="no"}
+export REGGAE=${REGGAE:="no"}
 
-
-if [ "${1}" = "reggae" ]; then
-  REGGAE="yes"
-fi
-
-
-if [ "${REGGAE}" = "yes" ]; then
-  backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}" hostname)
-  frontend_hostname=$(sudo cbsd jexec user=devel "jname=${frontend_app_name}front" hostname)
-  sudo tmux new-session -s "${backend_app_name}" -d "make -C services/backend devel"
-  sudo tmux split-window -h -p 66 -t 0 "make -C services/frontend BACKEND_URL=http://${backend_hostname}:5000 devel"
-  sudo tmux split-window -h -p 50 -t 1 "make -C services/startkit BACKEND_URL=http://${backend_hostname}:5000 devel"
-  if [ "${DESIGNER}" != "no" ]; then
-    sudo tmux split-window -v -p 50 -t 0 "make -C services/designer devel"
+for service in $@; do
+  if [ "backend" = "${service}" ]; then
+    export backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}" hostname)
   fi
+done
+
+export service=$1
+shift
+if [ "${REGGAE}" = "yes" ]; then
+  sudo tmux new-session -s "${backend_app_name}" -d "make -C services/${service} devel"
+else
+  "${BIN_DIR}/download_repos.sh"
+  tmux new-session -s "${backend_app_name}" -d "env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} ${PROJECT_ROOT}/services/${service}/bin/init.sh && env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} ${PROJECT_ROOT}/services/${service}/bin/devel.sh"
+fi
+export service=$1
+while [ ! -z "${service}" ]; do
+  if [ "${REGGAE}" = "yes" ]; then
+    sudo tmux split-window -h -p 50 -t 0 "make -C services/${service} BACKEND_URL=http://${backend_hostname}:5000 devel"
+  else
+    tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} ${PROJECT_ROOT}/services/${service}/bin/init.sh && env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 ${PROJECT_ROOT}/services/${service}/bin/devel.sh"
+  fi
+  shift
+  export service=$1
+done
+if [ "${REGGAE}" = "yes" ]; then
+  sudo tmux select-layout tiled
   sudo tmux a -t "${backend_app_name}"
 else
-  backend_hostname='localhost'
-  "${BIN_DIR}/download_repos.sh"
-  env OFFLINE=${OFFLINE} "${PROJECT_ROOT}/services/backend/bin/init.sh"
-  tmux new-session -s "${backend_app_name}" -d "env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} ${PROJECT_ROOT}/services/backend/bin/devel.sh"
-  tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} \"${PROJECT_ROOT}/services/frontend/bin/init.sh && env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 ${PROJECT_ROOT}/services/frontend/bin/devel.sh\""
-  if [ "${LIB}" != "no" ]; then
-    tmux split-window -v -p 50 -t 1 "env OFFLINE=${OFFLINE} \"${PROJECT_ROOT}/services/lib/bin/init.sh && env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 ${PROJECT_ROOT}/services/lib/bin/devel.sh\""
-  fi
+  tmux select-layout tiled
   tmux a -t "${backend_app_name}"
 fi
