e79367a05bd806afacdedc7c1fb6c426c0e73f99
Goran Mekić <meka@tilda.center>
2021-11-03 19:35:38 +0100

    Designer Svelte

--- bin/devel.sh
+++ bin/devel.sh
@@ -8,6 +8,7 @@ export OFFLINE=${OFFLINE:="no"}
 export SYSPKG=${SYSPKG:="no"}
 RAW_SERVICES=$@
 export SERVICES=${RAW_SERVICES:="backend frontend"}
+firstone="yes"
 
 if [ "${REGGAE}" != "yes" ]; then
   "${BIN_DIR}/download_repos.sh"
@@ -15,9 +16,10 @@ fi
 
 for service in ${SERVICES}; do
   if [ "backend" = "${service}" ]; then
+    firstone="no"
     if [ "${REGGAE}" = "yes" ]; then
       export backend_hostname=$(sudo cbsd jexec user=devel "jname=${backend_app_name}" hostname)
-      sudo tmux new-session -s "${backend_app_name}" -d "make -C services/${service} devel || sleep 60"
+      sudo tmux new-session -s "${backend_app_name}" -d "make -C services/${service} devel"
     else
       export backend_hostname="localhost"
       tmux new-session -s "${backend_app_name}" -d "env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} ${PROJECT_ROOT}/services/${service}/bin/devel.sh"
@@ -29,10 +31,19 @@ for service in ${SERVICES}; do
   if [ "backend" = "${service}" ]; then
     continue
   fi
-  if [ "${REGGAE}" = "yes" ]; then
-    sudo tmux split-window -h -p 50 -t 0 "make -C services/${service} BACKEND_URL=http://${backend_hostname}:5000 devel"
+  if [ "${firstone}" = "yes" ]; then
+    firstone="no"
+    if [ "${REGGAE}" = "yes" ]; then
+      sudo tmux new-session -s "${backend_app_name}" -d "make -C services/${service} devel || sleep 60"
+    else
+      tmux new-session -s "${backend_app_name}" -d "env OFFLINE=${OFFLINE} SYSPKG=${SYSPKG} ${PROJECT_ROOT}/services/${service}/bin/devel.sh"
+    fi
   else
-    tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 ${PROJECT_ROOT}/services/${service}/bin/devel.sh"
+    if [ "${REGGAE}" = "yes" ]; then
+      sudo tmux split-window -h -p 50 -t 0 "make -C services/${service} BACKEND_URL=http://${backend_hostname}:5000 devel || sleep 60"
+    else
+      tmux split-window -h -p 50 -t 0 "env OFFLINE=${OFFLINE} BACKEND_URL=http://${backend_hostname}:5000 ${PROJECT_ROOT}/services/${service}/bin/devel.sh"
+    fi
   fi
 done
 
